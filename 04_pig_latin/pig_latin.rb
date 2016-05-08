1147d077ed4ff2337eb0c15a77192210529c656d
Bryan Hunt <bryan.hunt@everbank.com>
2016-05-08 15:10:38 -0400

    adding pig latin completed tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,44 +1,28 @@
-def translate(words)
-  vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
-  schoolRule = ['s', 'c', 'h']
-  myWord = []
-  translated = []
-
-  # split the sentence into words
-  mySentence = words.split(" ")
-
-  # split each word into letters
-  mySentence.each do |w|
-    myWord.push(w.split(//)) 
-
-    # check the first three letters
-    if (w[0,1] == 's') && (w[1,1] == 'c')  && (w[2,1] == 'h')
-      myWord.push(w)
-    end
-  end
-  
-  myWord.each do |l|
-    if vowels.include?(l[0])
-      pigsay = l.join + "ay"
-    elsif consonants.include?(l[0]) && consonants.include?(l[1])
-      firstTwo = l[0..1]
-      l.push(firstTwo)
-      l.shift(2)
-      pigsay = l.join + "ay"
-    else
-      firstConsonant = l[0]
-      l.push(firstConsonant)
-      l.shift
-      pigsay = l.join + "ay"
-    end
-
-    puts pigsay
-    # translated.push(pigsay)
-  end
-
-  puts translated.join(' ')
-  
+def translate(allWords)
+  sentence = allWords.split(/\s/)
+  sentence.map!{ |x| translateOne(x) }
+  translated = sentence.join(" ")
+  return translated
 end
 
-translate("school")
+def translateOne(word)
+  vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+  if vowels.include?(word[0])
+    pigSpeak = word + "ay"
+  elsif (!vowels.include?(word[0])) && ((word[0] == "q") && (word[1] == "u"))
+    pigSpeak = word[2..-1] + word[0..1] + "ay"
+  elsif (!vowels.include?(word[0])) && ((word[1] == "q") && (word[2] == "u"))
+    pigSpeak = word[3..-1] + word[0..2] + "ay"
+  elsif (!vowels.include?(word[0])) && (!vowels.include?(word[1])) && (!vowels.include?(word[2]))
+    pigSpeak = word[3..-1] + word[0..2] + "ay"
+  elsif (!vowels.include?(word[0])) && (!vowels.include?(word[1]))
+    pigSpeak = word[2..-1] + word[0..1] + "ay"
+  else
+    pigSpeak = word[1..-1] + word[0] + "ay"
+  end
+
+  return pigSpeak
+end
+
+translate("three")
